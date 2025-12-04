59134ca180d4a0b7fce7a56398ef365f18cfebc7
jesuslab135 <111034145+jesuslab135@users.noreply.github.com>
2025-12-04 02:29:44 -0800

    Update ASGI configuration to use JWT middleware

--- WearableApi/WearableApi/asgi.py
+++ WearableApi/WearableApi/asgi.py
@@ -6,7 +6,6 @@ Supports both HTTP and WebSocket protocols.
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WearableApi.settings')
@@ -15,13 +14,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WearableApi.settings')
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-# Import routing after Django setup
+# Import routing and middleware after Django setup
 from api import routing
+from api.middleware import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
+        JWTAuthMiddlewareStack(
             URLRouter(
                 routing.websocket_urlpatterns
             )
